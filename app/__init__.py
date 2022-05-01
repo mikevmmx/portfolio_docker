025739d1447e127654530454cfd013655a990183
mike <mhernanez95@gmail.com>
2022-04-30 19:41:26 -0500

    prueba del portafio en docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 import os
+
 from flask import Flask
+
+import portfolio
 
 
 def create_app():
-    app= Flask(__name__)
+    app = Flask(__name__)
 
     app.config.from_mapping(
-        
         SENDGRID_KEY=os.environ.get("SENDGRID_API_KEY"),
-
-
     )
-
-    from . import portfolio
 
     app.register_blueprint(portfolio.bp)
 
-    return app
+    app.run(host="0.0.0.0", port=4000, debug=True)
+
+
+if __name__ == "__main__":
+    create_app()
--- app/portfolio.py
+++ app/portfolio.py
@@ -1,50 +1,47 @@
 from unicodedata import name
-from flask import(
-    Blueprint,
-    current_app,
-    redirect,
-    render_template,
-    request,
-    redirect,
-    url_for
-)
+
 import sendgrid
+from flask import (Blueprint, current_app, redirect, render_template, request,
+                   url_for)
 from sendgrid.helpers.mail import *
 
-bp=Blueprint('portfolio',__name__,url_prefix='/')
+from __init__ import create_app
 
-@bp.route('/',methods=['GET'])
+bp = Blueprint("portfolio", __name__, url_prefix="/")
+
+
+@bp.route("/", methods=["GET"])
 def index():
-    return render_template('portfolio/index.html')
+    return render_template("portfolio/index.html")
 
 
-@bp.route('/mail',methods=['POST','GET'])
+@bp.route("/mail", methods=["POST", "GET"])
 def mail():
-    name=request.form.get('name')
-    email=request.form.get('email')
-    message=request.form.get('message')
+    name = request.form.get("name")
+    email = request.form.get("email")
+    message = request.form.get("message")
 
-    if request.method == 'POST':
-        send_email(name,email,message)
-        return render_template('portfolio/send_mail.html')
-    return redirect(url_for('portfolio.index')) 
-
-
-
-def send_email(name,email,message):
-    mi_email='mhernanez95@gmail.com'
-    sg=sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
-
-    from_email=Email(mi_email)
-    to_email=To(mi_email,substitutions={
-        "-name-":name,
-        "-email-": email,
-        "-message-": message,
+    if request.method == "POST":
+        send_email(name, email, message)
+        return render_template("portfolio/send_mail.html")
+    return redirect(url_for("portfolio.index"))
 
 
-    })
+def send_email(name, email, message):
+    mi_email = "mhernanez95@gmail.com"
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config["SENDGRID_KEY"])
 
-    html_content="""
+    from_email = Email(mi_email)
+    to_email = To(
+        mi_email,
+        substitutions={
+            "-name-": name,
+            "-email-": email,
+            "-message-": message,
+        },
+    )
+
+    html_content = """
 
 
     <p>Hola Mike, tienes un nuevo contacto desde la web:</p>
@@ -53,7 +50,8 @@ def send_email(name,email,message):
     <p>Message: -message- </p>
     
     """
-    mail=Mail(mi_email,to_email,"Nuevo contacto desde la web",html_content=html_content)
-    response=sg.client.mail.send.post(request_body=mail.get())
-
+    mail = Mail(
+        mi_email, to_email, "Nuevo contacto desde la web", html_content=html_content
+    )
+    response = sg.client.mail.send.post(request_body=mail.get())
 
